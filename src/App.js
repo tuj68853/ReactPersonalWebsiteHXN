601b037548a845ac1c7bf69f94ebbbb5ca403403
Hai Nguyen <tuj68853@temple.edu>
2023-07-05 22:52:18 -0400

    Updated
    not personalized

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import './App.css';
-//import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+// components
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import About from './components/About';
+import Services from './components/Services';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
-
-function App() {
+const App = () => {
   return (
-  <div className="App">
-
-    <p>
-
-      hey guys 
-    </p>
-
-
-
-
-  </div>
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      {/*<div className='h-[4000px]'></div>*/}
+    </div>
   );
-}
+};
 
 export default App;
